7604e4ca798812398390868f74bb106579e4d216
DarkMagicSauce <35950530+DarkMagicSource@users.noreply.github.com>
2021-02-27 22:12:47 +1030

    Add Eckspei Locker textures

--- src/main/java/com/darkmagicsauce/ttb/blocks/EckspeiLocker.java
+++ src/main/java/com/darkmagicsauce/ttb/blocks/EckspeiLocker.java
@@ -1,9 +1,21 @@
 package com.darkmagicsauce.ttb.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.IntProperty;
 
-public class EckspeiLocker extends Block {
+public class EckspeiLocker extends Machine {
+	public static final IntProperty XPLEVEL = IntProperty.of("xp_level", 0, 5);
+
 	public EckspeiLocker(Settings settings) {
 		super(settings);
+		setDefaultState(this.stateManager.getDefaultState().with(XPLEVEL, 0));
+	}
+
+	@Override
+	protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
+		super.appendProperties(stateManager);
+		stateManager.add(XPLEVEL);
 	}
 }
