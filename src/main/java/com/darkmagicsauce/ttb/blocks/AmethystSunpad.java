8a68e8dee14cfd5b893d24efbe8e8e08b2ec6775
DarkMagicSauce <35950530+DarkMagicSource@users.noreply.github.com>
2021-02-27 16:21:15 +1030

    Work on Amethyst Sunpad

--- src/main/java/com/darkmagicsauce/ttb/blocks/AmethystSunpad.java
+++ src/main/java/com/darkmagicsauce/ttb/blocks/AmethystSunpad.java
@@ -1,9 +1,20 @@
 package com.darkmagicsauce.ttb.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
 
 public class AmethystSunpad extends Block {
+	public static final BooleanProperty POWERED = BooleanProperty.of("powered");
+
 	public AmethystSunpad(Settings settings) {
 		super(settings);
+		setDefaultState(this.stateManager.getDefaultState().with(POWERED, false));
+	}
+
+	@Override
+	protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
+		stateManager.add(POWERED);
 	}
 }
