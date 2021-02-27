49e467e5d35a78986df787888de927037410d5f1
DarkMagicSauce <35950530+DarkMagicSource@users.noreply.github.com>
2021-02-27 23:20:16 +1030

    Added Tempite Stone textures

--- src/main/java/com/darkmagicsauce/ttb/blocks/TempiteStone.java
+++ src/main/java/com/darkmagicsauce/ttb/blocks/TempiteStone.java
@@ -1,9 +1,20 @@
 package com.darkmagicsauce.ttb.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.IntProperty;
 
 public class TempiteStone extends Block {
+	public static final IntProperty POWER = IntProperty.of("power", 0, 2);
+
 	public TempiteStone(Settings settings) {
 		super(settings);
+		setDefaultState(this.stateManager.getDefaultState().with(POWER, 0));
+	}
+
+	@Override
+	protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
+		stateManager.add(POWER);
 	}
 }
