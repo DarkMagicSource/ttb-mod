3f8d29024e8c511ef422a90611b70f76698dda16
DarkMagicSauce <35950530+DarkMagicSource@users.noreply.github.com>
2021-02-28 11:25:17 +1030

    Add Tempite Insulator Textures

--- src/main/java/com/darkmagicsauce/ttb/blocks/TempiteInsulator.java
+++ src/main/java/com/darkmagicsauce/ttb/blocks/TempiteInsulator.java
@@ -1,9 +1,21 @@
 package com.darkmagicsauce.ttb.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 
 public class TempiteInsulator extends Block {
+	private static VoxelShape shape = Block.createCuboidShape(1.0D, 0.0D, 1.0D, 15.0D, 1.0D, 15.0D);
+
 	public TempiteInsulator(Settings settings) {
 		super(settings);
+	}
+
+	@Override
+	public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
+		return shape;
 	}
 }
